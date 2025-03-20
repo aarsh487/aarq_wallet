e06c7fa6ef01b7170d2bdf947dffb1ecf0eecdd8
aarsh487 <aanandpatel487@gmail.com>
2025-03-20 21:14:01 +0530

    add animations

--- src/components/Wallet.tsx
+++ src/components/Wallet.tsx
@@ -6,6 +6,7 @@ import nacl from "tweetnacl";
 import bs58 from "bs58";
 import { HDNodeWallet, Wallet } from "ethers";
 import { Eye, EyeOff } from "lucide-react";
+import { motion } from "motion/react"
 
 interface WalletType {
   publicKey: string;
@@ -105,36 +106,43 @@ export const WebWallet = () => {
     }
   };
 
+  const walletVariants = {
+  hidden: { opacity: 0, y: 20 },
+  visible: { opacity: 1, y: 0, transition: { duration: 0.4 } },
+};
+
+
   return (
     <div className="">
       {wallets.length === 0 && (
-        <div className="mt-40">
+        <div
+         className="mt-40">
           {pathTypes.length === 0 && (
-            <div className="flex justify-center items-center">
+            <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1, transition: { duration: 2 } }} className="flex justify-center items-center">
               <div className="">
                 <h1 className="mb-8 text-2xl md:text-4xl lg:text-6xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-gray-300 to-gray-500 text-center tracking-wide">
                   Pick your chain, Forge your path
                 </h1>
                 <div className="flex gap-4 justify-center items-center">
-                  <button
+                  <motion.button whileHover={{ scale: 1.1 }} transition={{ duration: 0.2 }} whileTap={{ scale: 0.95 }}
                     onClick={() => setPathTypes(["501"])}
                     className="px-4 py-2 md:py-3 md:px-6 rounded-lg cursor-pointer bg-white/10 backdrop-blur-md border border-white/20 text-white hover:bg-white/20 transition"
                   >
                     Solana
-                  </button>
-                  <button
+                  </motion.button>
+                  <motion.button whileHover={{ scale: 1.1 }} transition={{ duration: 0.2 }} whileTap={{ scale: 0.95 }}
                     onClick={() => setPathTypes(["60"])}
                     className="px-4 py-2 md:py-3 md:px-6 rounded-lg cursor-pointer bg-white/10 backdrop-blur-md border border-white/20 text-white hover:bg-white/20 transition"
                   >
                     Ethereum
-                  </button>
+                  </motion.button>
                 </div>
               </div>
-            </div>
+            </motion.div>
           )}
 
           {pathTypes.length !== 0 && (
-            <div className="flex justify-center items-center">
+            <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1, transition: { duration: 1 } }} className="flex justify-center items-center">
               <div className="space-y-20">
                 <h1 className="mb-2 text-4xl md:text-6xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-gray-300 to-gray-500 text-center tracking-wide">
                   Secure Seed Phrase
@@ -150,43 +158,43 @@ export const WebWallet = () => {
                     placeholder="12 words to rule them all..."
                     className="p-4 backdrop-blur-md border border-white/20 text-white"
                   />
-                  <button
+                  <motion.button whileHover={{ scale: 1.1 }} transition={{ duration: 0.2 }} whileTap={{ scale: 0.95 }}
                     onClick={() => handleGenerateWallet()}
                     className="px-6 py-3 rounded-lg cursor-pointer bg-white/10 backdrop-blur-md border border-white/20 text-white hover:bg-white/20 transition"
                   >
                     {mneumonic ? "Add Wallet" : "Create Wallet"}
-                  </button>
+                  </motion.button>
                 </div>
                 <p className="-mt-16 text-md md:text-sm text-gray-300 text-center">
                   Oops, No Seed? No Problem! Let Fate Generate One!
                 </p>
               </div>
-            </div>
+            </motion.div>
           )}
         </div>
       )}
 
       {wallets.length > 0 && (
-        <div className="mt-10">
+        <motion.div  initial={{ opacity: 0 }} animate={{ opacity: 1, transition: { duration: 0.5 } }} className="mt-10">
           <div className="flex gap-4 justify-center items-center">
-            <button
+            <motion.button whileHover={{ scale: 1.1 }} transition={{ duration: 0.2 }} whileTap={{ scale: 0.95 }}
               className="px-6 py-3 rounded-lg cursor-pointer bg-white/10 backdrop-blur-md border border-white/20 text-white hover:bg-white/20 transition"
               onClick={handleAddWallet}
             >
               Add Wallet
-            </button>
-            <button
+            </motion.button>
+            <motion.button whileHover={{ scale: 1.1 }} transition={{ duration: 0.2 }} whileTap={{ scale: 0.95 }}
               className="px-6 py-3 rounded-lg cursor-pointer bg-white/10 backdrop-blur-md border border-white/20 text-white hover:bg-white/20 transition"
               onClick={() => setShowMnemonic(true)}
             >
               Show Mneumonic
-            </button>
+            </motion.button>
           </div>
           <div className="lg:px-40 md:px-20 px-4 mt-20">
             <h2 className="lg:text-3xl text-2xl font-bold mb-4">Your Wallets</h2>
             <div className="grid grid-cols-1 md:grid-cols-1 gap-4">
               {wallets.map((wallet, index) => (
-                <div
+                <motion.div variants={walletVariants} initial="hidden" animate="visible"
                   key={index}
                   className="relative p-8 bg-gray-800 rounded-lg border border-gray-700"
                 >
@@ -204,16 +212,16 @@ export const WebWallet = () => {
                     </span>
                   </p>
                   <button className="absolute cursor-pointer bottom-8 right-8" onClick={() => setShowPrivateKey(!showPrivateKey)}>{showPrivateKey ? <Eye size={20} /> : <EyeOff size={20} />}</button>
-                </div>
+                </motion.div>
               ))}
             </div>
           </div>
-        </div>
+        </motion.div>
       )}
       <div>
         {showMnemonic && (
-          <div className="">
-            <div className="fixed inset-0 flex justify-center items-center bg-black/20 backdrop-blur-xs  bg-opacity-50">
+          <div  className="">
+            <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1, transition: { duration: 0.5 } }} className="fixed inset-0 flex justify-center items-center bg-black/20 backdrop-blur-xs  bg-opacity-50">
               <div className="bg-gray-800 p-6 rounded-lg lg:max-w-[600px] max-w-[400px] text-center relative">
                 <button
                   onClick={() => setShowMnemonic(false)}
@@ -232,14 +240,8 @@ export const WebWallet = () => {
                     </span>
                   ))}
                 </div>
-                {/* <button
-                  onClick={() => setShowMnemonic(false)}
-                  className="px-6 py-3 rounded-lg cursor-pointer bg-white/10 backdrop-blur-md border border-white/20 text-white hover:bg-white/20 transition"
-                >
-                  Copy
-                </button> */}
               </div>
-            </div>
+            </motion.div>
           </div>
         )}
       </div>
